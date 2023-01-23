4bd5fd238b949b4e060f46280741ffcb4af854e6
Leandro Herdy <leandro@DESKTOP-1QG6RTV>
2023-01-23 11:49:56 -0300

    Menssageria com RabbitMQ.

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,33 @@
+package com.ms.email.models;
+
+import com.ms.email.enums.StatusEmail;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private UUID emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+}
